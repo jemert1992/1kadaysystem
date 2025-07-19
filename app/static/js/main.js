a557ecf6be3f5ef57b717418787404e3209a8a24
jemert1992 <justin@tapp.co>
2025-07-19 09:58:50 -0400

    Create main.js placeholder

---
+++ app/static/js/main.js
@@ -0,0 +1,8 @@
+// JavaScript for 1kadaysystem
+
+// DOM ready function
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('1kadaysystem main.js loaded');
+    
+    // Add your JavaScript functionality here
+});
